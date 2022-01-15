6db07308ba292a6b6975c7fdd82697f5ffa3db88
Sven Liebert <mail@sven-liebert.de>
2022-01-15 20:42:55 +0100

    Initial

---
+++ src/useCourseList.tsx
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+export type Course = {
+    id: number,
+    description: string
+}
+
+export const useCourseList = () => {
+    const [courses, setCourses] = useState<Array<Course>>([])
+
+    const fetchCourseList = () => {
+        fetch('https://api.sven-liebert.de/htw-dresden/pillnitz/courses').then(response => {
+
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        }).then(
+            (courses: Array<Course>) => setCourses(courses)
+        )
+    }
+
+    return {courses, fetchCourseList}
+}
