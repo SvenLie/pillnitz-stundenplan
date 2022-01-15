156dd10d2112cd6a62e52dfbe21c1e45c41c3ca2
Sven Liebert <mail@sven-liebert.de>
2022-01-15 21:30:59 +0100

    Cleanup

---
+++ src/Impressum.tsx
@@ -0,0 +1,20 @@
+const Impressum = () => {
+    return (
+        <div className="App-content">
+            <br/>
+            <h4>Impressum</h4>
+            <br/>
+            <h6>Angaben gemäß § 5 TMG</h6>
+            <p>Sven Liebert<br/>
+                Gasteiner Str. 11<br/>
+                01279 Dresden</p>
+
+            <h6>Kontakt</h6>
+            <p>E-Mail: mail@sven-liebert.de</p>
+
+            <p>Quelle: <a href="https://www.e-recht24.de">eRecht24</a></p>
+        </div>
+    )
+}
+
+export default Impressum
